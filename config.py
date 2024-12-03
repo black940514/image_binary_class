7a24274757b172fa0c0a0eee28c6bbfb576ff0be
ty0514 <dkssud4352@gmail.com>
2024-12-03 19:25:16 +0900

    santa

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
 
-path = '/Users/taeyoun/Downloads/DeepLearning/project/Chapter 1. Binary Classification _ 두 선택지 중 하나를 선택하여 판별하는 분류 방법/실습 파일/2'
+path = '/Users/taeyoun/Downloads/DeepLearning/project/Chapter 1. Binary Classification _ 두 선택지 중 하나를 선택하여 판별하는 분류 방법/실습 파일/'
